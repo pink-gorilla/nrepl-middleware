7c70db0b76d04c0c261171bf359040f9627a88df
Andreas Steffan <a.steffan@contentreich.de>
2016-11-24 14:49:17 +0100

    - init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to gorilla-middleware
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/gorilla_middleware/render_values_test.clj
@@ -0,0 +1,7 @@
+(ns gorilla-middleware.render_values-test
+  (:require [clojure.test :refer :all]
+            [gorilla-middleware.render-values  :refer :all]))
+
+#_(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
