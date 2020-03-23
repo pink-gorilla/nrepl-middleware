ec7c1e49a6e69430b0f0c5c024dd33a82b876917
CI/CD <ci@pinkgorilla.org>
2020-03-23 18:52:18 +0000

    Begin 0.2.20-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/gorilla-middleware "0.2.19"
+(defproject org.pinkgorilla/gorilla-middleware "0.2.20-SNAPSHOT"
   :description "Pink Gorilla nREPL middleware"
   :url "https://github.com/pink-gorilla/gorilla-middleware"
   :license {:name "MIT"}
