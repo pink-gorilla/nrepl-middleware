d313ed5801c29c8634a281cf991b0e9a68fa17b0
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-02-21 11:43:38 -0500

    cljs handler fixed

--- src/pinkgorilla/nrepl/handler/cljs.clj
+++ src/pinkgorilla/nrepl/handler/cljs.clj
@@ -1,20 +1,10 @@
 (ns pinkgorilla.nrepl.handler.cljs
-  "Experimental CLJS - not quite sure if that makes sense at all"
-  (:require
-   [pinkgorilla.nrepl.handler.nrepl-handler :refer [nrepl-handler]]
-   [pinkgorilla.nrepl.middleware.sandboxed_interruptible-eval]
-   [pinkgorilla.nrepl.middleware.render-values :as render-mw] ;; it's essential this import comes after the previous one! It
-    ;; refers directly to a var in nrepl (as a hack to workaround
-    ;; a weakness in nREPL's middleware resolution).
-   ;; [clojure.tools.nrepl.server :as server]
-   ;; [cider.piggieback :as pb]
-   ))
+  "Experimental CLJS - not quite sure if that makes sense at all")
 
-(def ^:private cljs-middleware
+(def middleware-cljs
   "A vector containing the cljs gorilla repl supports."
   '[cider.piggieback/wrap-cljs-repl])
 
-;; TODO middleware must resolve
-(defn cljs-handler []
-  (require 'cider.piggieback)
-  (nrepl-handler false cljs-middleware))
+(defn require-cljs []
+  ;; force side effects at runtime in nrepl-process
+  (require 'cider.piggieback))
