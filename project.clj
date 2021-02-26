ac6c0cc0e36fd33aa3f7eb952643cfbba95588ce
CI/CD <ci@pinkgorilla.org>
2021-02-26 18:47:50 +0000

    Release 0.3.15

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/nrepl-middleware "0.3.15-SNAPSHOT"
+(defproject org.pinkgorilla/nrepl-middleware "0.3.15"
   :description "nREPL middleware"
   :url "https://github.com/pink-gorilla/nrepl-middleware"
   :license {:name "MIT"}
